c49f84ad49cc9bf058af18a4da68c1006e77f990
Scott Walker <scott.walker@valtech.co.uk>
2013-07-18 23:01:15 +0100

    using mkString prefix and suffix params to make code more readable

--- app/nodes/Method.scala
+++ app/nodes/Method.scala
@@ -3,7 +3,7 @@ package nodes
 case class Method() extends Node {
   def isTerminal: Boolean = false
   def canTerminate(stepsRemaining: Integer): Boolean = ???
-  def toRawScala: String = s"def ${name}(${params.mkString(", ")}) = { ${nodes.map(f=>f.toRawScala).mkString(" ")} }"
+  def toRawScala: String = s"def ${name}${params.mkString("(", ", ", ")")} = ${nodes.map(f=>f.toRawScala).mkString("{ ", " ", " }")}"
   val name = "f1"
   val params: Seq[String] = Seq("a: Int", "b: Int")
   val nodes: Seq[Node] = Seq(AddOperator())
--- app/nodes/ObjectM.scala
+++ app/nodes/ObjectM.scala
@@ -3,7 +3,7 @@ package nodes
 case class ObjectM() extends Node {
   def isTerminal: Boolean = false
   def canTerminate(stepsRemaining: Integer): Boolean = ???
-  def toRawScala: String = s"object ${name} { ${nodes.map(f=>f.toRawScala).mkString(" ")} }"
+  def toRawScala: String = s"object ${name} ${nodes.map(f=>f.toRawScala).mkString("{ ", " ", " }")}"
   val name = "Individual"
   val nodes: Seq[Node] = Seq(Method())
 }
