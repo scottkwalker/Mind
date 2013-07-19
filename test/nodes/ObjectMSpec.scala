c09c72b0d0c5bae54f35f44560005741aca6eb4d
Scott Walker <scott.walker@valtech.co.uk>
2013-07-19 18:22:30 +0100

    ObjectMSpec

---
+++ test/nodes/ObjectMSpec.scala
@@ -0,0 +1,51 @@
+package nodes
+
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+
+class ObjectMSpec extends Specification {
+  "ObjectM" should {
+    "is not terminal" in {
+      val objectM = ObjectM(Seq(Method(Seq(AddOperator(Value("a"), Value("b"))))))
+      
+      objectM.isTerminal mustEqual false
+    }
+    
+    "canTerminate in 4 steps" in {
+      val objectM = ObjectM(Seq(Method(Seq(AddOperator(Value("a"), Value("b"))))))
+      
+      objectM.canTerminate(4) mustEqual true
+    }
+    
+    "can not terminate in 3 steps" in {
+      val objectM = ObjectM(Seq(Method(Seq(AddOperator(Value("a"), Value("b"))))))
+      
+      objectM.canTerminate(3) mustEqual false
+    }
+    
+    "validate true when does not contain any empty nodes" in {
+      val objectM = ObjectM(Seq(Method(Seq(AddOperator(Value("a"), Value("b"))))))
+      
+      objectM.validate mustEqual true
+    }
+    
+    "validate false given" in {
+      "single empty method node" in {
+        val objectM = ObjectM(Seq(Empty()))
+        objectM.validate mustEqual false
+      }
+      
+      "empty method node in a sequence" in {
+        val objectM = ObjectM(Seq(Method(Seq(AddOperator(Value("a"), Value("b")))), Empty()))
+        objectMp.validate mustEqual false
+      }
+    }
+    
+    "toRawScala" in {
+      val objectM = ObjectM(Seq(Method(Seq(AddOperator(Value("a"), Value("b"))))))
+      
+      objectM.toRawScala mustEqual "object Individual { def f1(a: Int, b: Int) = { a + b } }"
+    }
+  }
+}
