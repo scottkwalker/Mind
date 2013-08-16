20dff1af35fa751a826ccdceaa6951947ac25837
Scott Walker <scott.walker@valtech.co.uk>
2013-08-16 09:43:13 +0100

    Add Scope spec

---
+++ test/nodes/helpers/ScopeSpec.scala
@@ -0,0 +1,49 @@
+package nodes.helpers
+
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+
+class ScopeSpec extends Specification {
+  "Scope" should {
+    "defauls values to zero" in {
+      Scope() must beLike {
+        case Scope(v, f, o) => {
+          v mustEqual 0
+          f mustEqual 0
+          o mustEqual 0
+        }
+      }
+    }
+
+    "incrementVals returns expected" in {
+      Scope().incrementVals must beLike {
+        case Scope(v, f, o) => {
+          v mustEqual 1
+          f mustEqual 0
+          o mustEqual 0
+        }
+      }
+    }
+
+    "incrementFuncs returns expected" in {
+      Scope().incrementFuncs must beLike {
+        case Scope(v, f, o) => {
+          v mustEqual 0
+          f mustEqual 1
+          o mustEqual 0
+        }
+      }
+    }
+
+    "incrementObjects returns expected" in {
+      Scope().incrementObjects must beLike {
+        case Scope(v, f, o) => {
+          v mustEqual 0
+          f mustEqual 0
+          o mustEqual 1
+        }
+      }
+    }
+  }
+}
