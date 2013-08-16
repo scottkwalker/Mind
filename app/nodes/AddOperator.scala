d3dd77e92297ee6b428ba8462d566ca38de9d27f
Scott Walker <scott.walker@valtech.co.uk>
2013-08-16 10:24:15 +0100

    More tests using mocks. Moved stepsRemaining check so it isn't repeatedly tested.

--- app/nodes/AddOperator.scala
+++ app/nodes/AddOperator.scala
@@ -7,12 +7,12 @@ case class AddOperator(val left: Node, val right: Node) extends Node {
   def toRawScala: String = s"${left.toRawScala} + ${right.toRawScala}"
 
   def validate(stepsRemaining: Integer): Boolean = {
-    validate(left, stepsRemaining) && validate(right, stepsRemaining)
+    if (stepsRemaining == 0) false
+    else validate(left, stepsRemaining) && validate(right, stepsRemaining)
   }
 
   private def validate(n: Node, stepsRemaining: Integer) = {
-    if (stepsRemaining == 0) false
-    else n match {
+    n match {
       case _: ValueRef => left.validate(stepsRemaining - 1)
       case _: Empty => false
       case _ => false
--- test/nodes/AddOperatorSpec.scala
+++ test/nodes/AddOperatorSpec.scala
@@ -9,44 +9,62 @@ import org.specs2.mock.Mockito
 class AddOperatorSpec extends Specification with Mockito {
   "AddOperator" should {
     "toRawScala" in {
-      AddOperator(ValueRef("a"), ValueRef("b")).toRawScala mustEqual "a + b"
+      val a = mock[ValueRef]
+      a.toRawScala returns "a"
+      val b = mock[ValueRef]
+      b.toRawScala returns "b"
+
+      AddOperator(a, b).toRawScala mustEqual "a + b"
     }
 
     "validate" in {
-      "true given it can terminates in under N steps" in {
-        AddOperator(ValueRef("a"), ValueRef("b")).validate(2) mustEqual true
+      "true given child nodes can terminate in under N steps" in {
+        val v = mock[ValueRef]
+        v.validate(anyInt) returns true
+        
+        AddOperator(v, v).validate(2) mustEqual true
+      }
+      
+      "false given it cannot terminate in 0 steps" in {
+        val v = mock[ValueRef]
+        v.validate(anyInt) returns false
+        
+        AddOperator(v, v).validate(0) mustEqual false
       }
 
-      "false given it cannot terminate in under N steps" in {
-        AddOperator(ValueRef("a"), ValueRef("b")).validate(1) mustEqual false
+      "false given child nodes cannot terminate in under N steps" in {
+        val v = mock[ValueRef]
+        v.validate(anyInt) returns false
+        
+        AddOperator(v, v).validate(10) mustEqual false
       }
 
       "true given none empty" in {
         val v = mock[ValueRef]
         v.validate(anyInt) returns true
-        
+
         AddOperator(v, v).validate(10) mustEqual true
       }
 
       "false given contains an empty node" in {
         val v = mock[ValueRef]
         v.validate(anyInt) returns true
-        
+
         AddOperator(v, Empty()).validate(10) mustEqual false
       }
 
       "false given contains a node that is not valid for this level" in {
         val v = mock[ValueRef]
         v.validate(anyInt) returns true
-        
+
         AddOperator(v, ObjectM(Nil)).validate(10) mustEqual false
       }
     }
-    
+
     "create returns instance of this type" in {
       val s = mock[Scope]
       s.numVals returns 0
-      
+
       AddOperator.create(scope = s) must beAnInstanceOf[AddOperator]
     }
   }
