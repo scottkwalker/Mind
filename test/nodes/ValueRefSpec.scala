680b819387079241214ae324ffd0f0ae711a761f
Scott Walker <scott.walker@valtech.co.uk>
2013-10-01 22:23:32 +0100

    Improved spec with name as a val

--- test/nodes/ValueRefSpec.scala
+++ test/nodes/ValueRefSpec.scala
@@ -7,30 +7,39 @@ import org.specs2.mock.Mockito
 class ValueRefSpec extends Specification with Mockito {
   "ValueRef" should {
     "toRawScala" in {
-      ValueRef("a").toRawScala mustEqual "a"
+      val name = "a"
+
+      ValueRef(name).toRawScala mustEqual name
     }
 
     "validate" in {
       "false given it cannot terminate in under N steps" in {
         val s = Scope(maxDepth = 0)
-        ValueRef("a").validate(s) mustEqual false
+        val name = "a"
+
+        ValueRef(name).validate(s) mustEqual false
       }
 
       "true given a non-empty name" in {
         val s = Scope(maxDepth = 10)
-        ValueRef("a").validate(s) mustEqual true
+        val name = "a"
+
+        ValueRef(name).validate(s) mustEqual true
       }
 
       "false given an empty name" in {
         val s = Scope(maxDepth = 10)
-        ValueRef("").validate(s) mustEqual false
+        val name = ""
+
+        ValueRef(name).validate(s) mustEqual false
       }
     }
 
     "replaceEmpty" in {
       "returns same when no wildcards" in {
         val s = mock[Scope]
-        val instance = ValueRef("a")
+        val name = "a"
+        val instance = ValueRef(name)
 
         instance.replaceEmpty(s) mustEqual instance
       }
