b01e33949d02c0c5bd6fa3436205b7ff80e3a590
Scott Walker <scott.walker@valtech.co.uk>
2013-10-24 16:50:16 +0100

    Fix compile problem caused by organise imports

--- app/ai/aco/Aco.scala
+++ app/ai/aco/Aco.scala
@@ -8,7 +8,8 @@ import com.google.inject.Inject
 
 case class Aco @Inject()(rng: Random) extends Ai {
   override def chooseChild(possibleChildren: Seq[CreateChildNodes]): CreateChildNodes = {
-    possibleChildren(0)
+    val index = rng.nextInt(possibleChildren.length)
+    possibleChildren(index)
   }
 
   override def chooseIndex(seqLength: Int): Int = 0
--- project/Build.scala
+++ project/Build.scala
@@ -1,10 +1,12 @@
 import sbt._
 import Keys._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName = "Mind"
-  val appVersion = "1.0-SNAPSHOT"
+  val appName         = "Mind"
+  val appVersion      = "1.0-SNAPSHOT"
+
 
 
   val appDependencies = Seq(
