d53f544a185cc072d01963690c88bd97dff5c928
Scott Walker <scott.walker@valtech.co.uk>
2013-12-30 14:44:48 +0000

    Now using a mock of Ai instead of a real instance

--- test/nodes/helpers/CreateSeqNodeScalaTestSpec.scala
+++ test/nodes/helpers/CreateSeqNodeScalaTestSpec.scala
@@ -9,7 +9,6 @@ import ai.aco.Aco
 
 class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
   "create" should {
-
     "call a factory once given only space for 1 func in obj and mocked rng the same" in {
       val expected = 1
 
@@ -18,18 +17,22 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
       val v = strictMock[ICreateChildNodes]
       val rng = strictMock[IRandomNumberGenerator]
       val cn = strictMock[ICreateNode]
+      val ai = strictMock[Ai]
 
-      val ai: Ai = Aco(rng)
       val sut = CreateSeqNodes(cn, rng, ai)
 
       expecting {
         s.maxFuncsInObject andReturn 1
+        ai.canAddAnother(anyInt(),
+          anyInt(),
+          anyObject[IRandomNumberGenerator]) andReturn true andReturn false
+
         cn.create(anyObject[Seq[ICreateChildNodes]],
           anyObject[IScope],
           anyObject[Ai]) andReturn ((s, n))
       }
 
-      whenExecuting(s, n, v, rng, cn) {
+      whenExecuting(s, n, v, rng, cn, ai) {
         // Act
         val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
           scope = s,
@@ -49,13 +52,15 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
       val v = strictMock[ICreateChildNodes]
       val rng = strictMock[IRandomNumberGenerator]
       val cn = strictMock[ICreateNode]
+      val ai = strictMock[Ai]
 
-      val ai: Ai = Aco(rng)
       val sut = CreateSeqNodes(cn, rng, ai)
 
       expecting {
         s.maxFuncsInObject andReturn 2
-        rng.nextBoolean() andReturn true
+        ai.canAddAnother(anyInt(),
+          anyInt(),
+          anyObject[IRandomNumberGenerator]) andReturn true andReturn true andReturn false
 
         (cn.create(anyObject[Seq[ICreateChildNodes]],
           anyObject[IScope],
@@ -63,7 +68,7 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
           times(2)
       }
 
-      whenExecuting(s, n, v, rng, cn) {
+      whenExecuting(s, n, v, rng, cn, ai) {
         // Act
         val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
           scope = s,
@@ -75,6 +80,41 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
       }
     }
 
+    "calls create on factory once given space for 2 func in obj but rng mocked to 1" in {
+      val expected = 1
+
+      val s = strictMock[IScope]
+      val n = strictMock[Node]
+      val v = strictMock[ICreateChildNodes]
+      val rng = strictMock[IRandomNumberGenerator]
+      val cn = strictMock[ICreateNode]
+      val ai = strictMock[Ai]
+
+      val sut = CreateSeqNodes(cn, rng, ai)
+
+      expecting {
+        s.maxFuncsInObject andReturn 2
+        ai.canAddAnother(anyInt(),
+          anyInt(),
+          anyObject[IRandomNumberGenerator]) andReturn true andReturn false
+
+        (cn.create(anyObject[Seq[ICreateChildNodes]],
+          anyObject[IScope],
+          anyObject[Ai]) andReturn ((s, n))).
+          once
+      }
+
+      whenExecuting(s, n, v, rng, cn, ai) {
+        // Act
+        val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
+          scope = s,
+          factoryLimit = s.maxFuncsInObject
+        )
+
+        // Assert
+        assert(nodes.length == expected)
+      }
+    }
 
     "calls create on factory once given space for 2 func in obj and a rng mocked to 2 but 1 pre-made node already added" in {
       val expected = 2
@@ -84,13 +124,15 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
       val v = strictMock[ICreateChildNodes]
       val rng = strictMock[IRandomNumberGenerator]
       val cn = strictMock[ICreateNode]
+      val ai = strictMock[Ai]
 
-      val ai: Ai = Aco(rng)
       val sut = CreateSeqNodes(cn, rng, ai)
 
       expecting {
         s.maxFuncsInObject andReturn 2
-        rng.nextBoolean() andReturn true
+        ai.canAddAnother(anyInt(),
+          anyInt(),
+          anyObject[IRandomNumberGenerator]) andReturn true andReturn false
 
         (cn.create(anyObject[Seq[ICreateChildNodes]],
           anyObject[IScope],
@@ -98,7 +140,7 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
           once
       }
 
-      whenExecuting(s, n, v, rng, cn) {
+      whenExecuting(s, n, v, rng, cn, ai) {
         // Act
         val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
           scope = s,
