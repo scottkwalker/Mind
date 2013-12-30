0283cc6614e2060243654f3cc962016c5351e0db
Scott Walker <scott.walker@valtech.co.uk>
2013-12-30 14:15:58 +0000

    Another test in ScalaTest EasyMock style

--- test/nodes/helpers/CreateSeqNodeScalaTestSpec.scala
+++ test/nodes/helpers/CreateSeqNodeScalaTestSpec.scala
@@ -76,5 +76,39 @@ class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
     }
 
 
+    "calls create on factory once given space for 2 func in obj and a rng mocked to 2 but 1 pre-made node already added" in {
+      val expected = 2
+
+      val s = strictMock[IScope]
+      val n = strictMock[Node]
+      val v = strictMock[ICreateChildNodes]
+      val rng = strictMock[IRandomNumberGenerator]
+      val cn = strictMock[ICreateNode]
+
+      val ai: Ai = Aco(rng)
+      val sut = CreateSeqNodes(cn, rng, ai)
+
+      expecting {
+        s.maxFuncsInObject andReturn 2
+        rng.nextBoolean() andReturn true
+
+        (cn.create(anyObject[Seq[ICreateChildNodes]],
+          anyObject[IScope],
+          anyObject[Ai]) andReturn ((s, n))).
+          once
+      }
+
+      whenExecuting(s, n, v, rng, cn) {
+        // Act
+        val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
+          scope = s,
+          acc = Seq(n), // Pre-made
+          factoryLimit = s.maxFuncsInObject
+        )
+
+        // Assert
+        assert(nodes.length == expected)
+      }
+    }
   }
 }
