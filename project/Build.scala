4ad78bb443380b59dc2230576e2b9c6967229633
Scott Walker <scott.walker@valtech.co.uk>
2014-03-06 22:39:33 +0000

    Upgraded twitter util-eval version

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object Build extends sbt.Build {
     anorm,
     "org.specs2" %% "specs2" % "2.3.6" % "test" withSources() withJavadoc(),
     "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc(), // For specs2
-    "com.twitter" % "util-eval_2.10" % "6.10.0",
+    "com.twitter" % "util-eval_2.10" % "6.12.1",
     "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
     "com.google.inject" % "guice" % "4.0-beta",
     "com.tzavellas" % "sse-guice" % "0.7.1",
