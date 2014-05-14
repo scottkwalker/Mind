083744112e9fa026614215bef522ec6472e337a0
Scott Walker <scott.walker@valtech.co.uk>
2014-05-14 22:26:40 +0100

    removed unused constructor injection

--- app/ai/legalGamer/LegalGamer.scala
+++ app/ai/legalGamer/LegalGamer.scala
@@ -5,12 +5,13 @@ import nodes.helpers._
 import com.google.inject.Inject
 
 // Always chooses the first legal move available
-case class LegalGamer @Inject()(rng: IRandomNumberGenerator) extends SelectionStrategy {
+case class LegalGamer @Inject()() extends SelectionStrategy {
   override def chooseChild(possibleChildren: Seq[ICreateChildNodes]): ICreateChildNodes = {
     require(possibleChildren.length > 0, "Sequence must not be empty otherwise we cannot pick an node from it")
     possibleChildren(0)
   }
 
+  // TODO doesn't need IRandomNumberGenerator as it will be on the constructor.
   override def canAddAnother(accLength: Int,
                              factoryLimit: Int,
                              rng: IRandomNumberGenerator): Boolean = accLength < factoryLimit
