ab04e57c157e5026772b9ca51267e4307717f8e5
Scott Walker <scott.walker@valtech.co.uk>
2014-05-21 23:27:49 +0100

    update Build for twitter, scalacheck and guice

--- project/Build.scala
+++ project/Build.scala
@@ -16,9 +16,9 @@ object Build extends sbt.Build {
     jdbc,
     anorm,
     "org.mockito" % "mockito-all" % "1.9.5" % "test" withSources() withJavadoc(), // For specs2
-    "com.twitter" % "util-eval_2.10" % "6.12.1",
-    "org.scalacheck" %% "scalacheck" % "1.10.1" % "test",
-    "com.google.inject" % "guice" % "4.0-beta",
+    "com.twitter" % "util-eval_2.10" % "6.16.0",
+    "org.scalacheck" %% "scalacheck" % "1.11.4" % "test",
+    "com.google.inject" % "guice" % "4.0-beta4",
     "com.tzavellas" % "sse-guice" % "0.7.1",
     "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
     "com.twitter" % "util-core_2.10" % "6.12.1"
