ffd8145d44a5fbb862292cc4f9de8f4b86390328
Scott Walker <scott.walker@valtech.co.uk>
2014-05-21 22:41:01 +0100

    removing refs to EasyMock

--- test/nodes/helpers/CreateSeqNodeScalaTestSpec.scala
+++ test/nodes/helpers/CreateSeqNodeScalaTestSpec.scala
@@ -1,147 +1,122 @@
 package nodes.helpers
 
-import org.scalatest.WordSpec
-import org.scalatest.mock.EasyMockSugar
-import org.easymock.EasyMock._
 import ai.IAi
 import models.domain.common.Node
+import utils.helpers.UnitSpec
+import org.mockito.Mockito._
+import org.mockito.Matchers._
 
-final class CreateSeqNodeScalaTestSpec extends WordSpec with EasyMockSugar {
+final class CreateSeqNodeScalaTestSpec extends UnitSpec {
   "create" should {
     "call a factory once given only space for 1 func in obj and mocked rng the same" in {
       val expected = 1
 
-      val s = strictMock[IScope]
-      val n = strictMock[Node]
-      val v = strictMock[ICreateChildNodes]
-      val cn = strictMock[ICreateNode]
-      val ai = strictMock[IAi]
+      val s = mock[IScope]
+      val n = mock[Node]
+      val v = mock[ICreateChildNodes]
+      val cn = mock[ICreateNode]
+      val ai = mock[IAi]
 
       val sut = CreateSeqNodes(cn, ai)
 
-      expecting {
-        s.maxFuncsInObject andReturn 1
-        ai.canAddAnother(anyInt(),
-          anyInt()) andReturn true andReturn false
+      when(s.maxFuncsInObject).thenReturn(1)
+      when(ai.canAddAnother(anyInt(),
+        anyInt())).thenReturn(true, false)
 
-        cn.create(anyObject[Seq[ICreateChildNodes]],
-          anyObject[IScope],
-          anyObject[IAi]) andReturn ((s, n))
-      }
+      when(cn.create(anyObject[Seq[ICreateChildNodes]],
+        anyObject[IScope],
+        anyObject[IAi])).thenReturn((s, n))
 
-      whenExecuting(s, n, v, cn, ai) {
-        // Act
-        val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
-          scope = s,
-          factoryLimit = s.maxFuncsInObject
-        )
+      // Act
+      val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
+        scope = s,
+        factoryLimit = s.maxFuncsInObject
+      )
 
-        // Assert
-        assert(nodes.length == expected)
-      }
+      // Assert
+      assert(nodes.length == expected)
     }
 
     "calls create on factory twice given space for 2 func in obj and mocked rng the same" in {
       val expected = 2
 
-      val s = strictMock[IScope]
-      val n = strictMock[Node]
-      val v = strictMock[ICreateChildNodes]
-      val cn = strictMock[ICreateNode]
-      val ai = strictMock[IAi]
+      val s = mock[IScope]
+      val n = mock[Node]
+      val v = mock[ICreateChildNodes]
+      val cn = mock[ICreateNode]
+      val ai = mock[IAi]
 
       val sut = CreateSeqNodes(cn, ai)
+      when(s.maxFuncsInObject).thenReturn(2)
+      when(ai.canAddAnother(anyInt(), anyInt())).thenReturn(true, true, false)
+      when(cn.create(anyObject[Seq[ICreateChildNodes]], anyObject[IScope], anyObject[IAi])).thenReturn((s, n))
 
-      expecting {
-        s.maxFuncsInObject andReturn 2
-        ai.canAddAnother(anyInt(),
-          anyInt()) andReturn true andReturn true andReturn false
+      // Act
+      val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
+        scope = s,
+        factoryLimit = s.maxFuncsInObject
+      )
 
-        (cn.create(anyObject[Seq[ICreateChildNodes]],
-          anyObject[IScope],
-          anyObject[IAi]) andReturn ((s, n))).
-          times(2)
-      }
-
-      whenExecuting(s, n, v, cn, ai) {
-        // Act
-        val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
-          scope = s,
-          factoryLimit = s.maxFuncsInObject
-        )
-
-        // Assert
-        assert(nodes.length == expected)
-      }
+      // Assert
+      assert(nodes.length == expected)
     }
 
     "calls create on factory once given space for 2 func in obj but rng mocked to 1" in {
       val expected = 1
 
-      val s = strictMock[IScope]
-      val n = strictMock[Node]
-      val v = strictMock[ICreateChildNodes]
-      val cn = strictMock[ICreateNode]
-      val ai = strictMock[IAi]
+      val s = mock[IScope]
+      val n = mock[Node]
+      val v = mock[ICreateChildNodes]
+      val cn = mock[ICreateNode]
+      val ai = mock[IAi]
 
       val sut = CreateSeqNodes(cn, ai)
 
-      expecting {
-        s.maxFuncsInObject andReturn 2
-        ai.canAddAnother(anyInt(),
-          anyInt()) andReturn true andReturn false
+      when(s.maxFuncsInObject).thenReturn(2)
+      when(ai.canAddAnother(anyInt(),
+        anyInt())).thenReturn(true, false)
 
-        (cn.create(anyObject[Seq[ICreateChildNodes]],
-          anyObject[IScope],
-          anyObject[IAi]) andReturn ((s, n))).
-          once
-      }
+      when(cn.create(anyObject[Seq[ICreateChildNodes]],
+        anyObject[IScope],
+        anyObject[IAi])).thenReturn((s, n))
 
-      whenExecuting(s, n, v, cn, ai) {
-        // Act
-        val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
-          scope = s,
-          factoryLimit = s.maxFuncsInObject
-        )
+      // Act
+      val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
+        scope = s,
+        factoryLimit = s.maxFuncsInObject
+      )
 
-        // Assert
-        assert(nodes.length == expected)
-      }
+      // Assert
+      assert(nodes.length == expected)
     }
 
     "calls create on factory once given space for 2 func in obj and a rng mocked to 2 but 1 pre-made node already added" in {
       val expected = 2
 
-      val s = strictMock[IScope]
-      val n = strictMock[Node]
-      val v = strictMock[ICreateChildNodes]
-      val cn = strictMock[ICreateNode]
-      val ai = strictMock[IAi]
+      val s = mock[IScope]
+      val n = mock[Node]
+      val v = mock[ICreateChildNodes]
+      val cn = mock[ICreateNode]
+      val ai = mock[IAi]
 
       val sut = CreateSeqNodes(cn, ai)
 
-      expecting {
-        s.maxFuncsInObject andReturn 2
-        ai.canAddAnother(anyInt(),
-          anyInt()) andReturn true andReturn false
+      when(s.maxFuncsInObject).thenReturn(2)
+      when(ai.canAddAnother(anyInt(), anyInt())).thenReturn(true, false)
 
-        (cn.create(anyObject[Seq[ICreateChildNodes]],
-          anyObject[IScope],
-          anyObject[IAi]) andReturn ((s, n))).
-          once
-      }
+      when(cn.create(anyObject[Seq[ICreateChildNodes]],
+        anyObject[IScope],
+        anyObject[IAi])).thenReturn((s, n))
 
-      whenExecuting(s, n, v, cn, ai) {
-        // Act
-        val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
-          scope = s,
-          acc = Seq(n), // Pre-made
-          factoryLimit = s.maxFuncsInObject
-        )
+      // Act
+      val (_, nodes) = sut.createSeq(possibleChildren = Seq(v),
+        scope = s,
+        acc = Seq(n), // Pre-made
+        factoryLimit = s.maxFuncsInObject
+      )
 
-        // Assert
-        assert(nodes.length == expected)
-      }
+      // Assert
+      assert(nodes.length == expected)
     }
   }
 }
