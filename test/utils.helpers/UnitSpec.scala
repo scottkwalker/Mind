9cfdbec329f535154ae738c5a2b4f6b6b1f2fdc0
Scott Walker <scott.walker@valtech.co.uk>
2014-05-21 22:42:18 +0100

    works for parallel tests but is unreadable so is commented out

--- test/utils.helpers/UnitSpec.scala
+++ test/utils.helpers/UnitSpec.scala
@@ -1,7 +1,7 @@
 package utils.helpers
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{ParallelTestExecution, Matchers, WordSpec}
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.concurrent.ScalaFutures
 
-abstract class UnitSpec extends WordSpec with Matchers with MockitoSugar with ScalaFutures
+abstract class UnitSpec extends WordSpec with Matchers with MockitoSugar with ScalaFutures //with ParallelTestExecution
