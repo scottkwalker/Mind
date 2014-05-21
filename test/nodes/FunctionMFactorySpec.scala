fa5bb6c724ce17caab805f5490c8d388a5180284
Scott Walker <scott.walker@valtech.co.uk>
2014-05-21 20:45:54 +0100

    converting from specs2 to UnitSpec

--- test/nodes/FunctionMFactorySpec.scala
+++ test/nodes/FunctionMFactorySpec.scala
@@ -1,57 +1,52 @@
 package nodes
 
-import org.specs2.mutable._
-import org.specs2.mock.Mockito
 import com.google.inject.{Injector, Guice}
 import nodes.helpers.{IScope, Scope}
 import ai.IRandomNumberGenerator
 import modules.ai.legalGamer.LegalGamerModule
 import modules.DevModule
 import models.domain.scala.FunctionM
+import utils.helpers.UnitSpec
+import org.mockito.Mockito._
+import org.mockito.Matchers._
 
-class FunctionMFactorySpec extends Specification with Mockito {
+class FunctionMFactorySpec extends UnitSpec {
   val rng = mock[IRandomNumberGenerator]
-  rng.nextInt(any[Int]) returns 2
-  rng.nextBoolean() returns true
+  when(rng.nextInt(any[Int])).thenReturn(2)
+  when(rng.nextBoolean()).thenReturn(true)
 
   val injector: Injector = Guice.createInjector(new DevModule(randomNumberGenerator = rng), new LegalGamerModule)
   val factory = injector.getInstance(classOf[FunctionMFactory])
 
-  "create" in {
-    "returns instance of this type" in {
+  "create" should {
+    "return instance of this type" in {
       val s = Scope(maxDepth = 10)
 
       val instance = factory.create(scope = s)
 
-      instance must beAnInstanceOf[FunctionM]
+      instance shouldBe a[FunctionM]
     }
 
-    "returns expected given scope with 0 functions" in {
+    "return expected given scope with 0 functions" in {
       val s = Scope(numFuncs = 0, maxDepth = 10)
 
       val instance = factory.create(scope = s)
 
-      instance must beLike {
-        case FunctionM(_, _, name) => name mustEqual "f0"
+      instance match {
+        case FunctionM(_, _, name) => name should equal("f0")
+        case _ => fail("wrong type")
       }
     }
 
-    "returns expected given scope with 1 functions" in {
+    "return expected given scope with 1 functions" in {
       val s = Scope(numFuncs = 1, maxDepth = 10)
 
       val instance = factory.create(scope = s)
 
-      instance must beLike {
-        case FunctionM(_, _, name) => name mustEqual "f1"
+      instance match {
+        case FunctionM(_, _, name) => name should equal( "f1")
+        case _ => fail("wrong type")
       }
-    }
-
-    "update scope calls increment functions" in {
-      val s = mock[IScope]
-
-      factory.updateScope(s)
-
-      there was one(s).incrementFuncs
     }
 
     "returns 3 children given scope with 3 maxExpressionsInFunc (and rng mocked)" in {
@@ -59,9 +54,20 @@ class FunctionMFactorySpec extends Specification with Mockito {
 
       val instance = factory.create(scope = s)
 
-      instance must beLike {
-        case FunctionM(_, children, _) => children.length mustEqual 3
+      instance match {
+        case FunctionM(_, children, _) => children.length should equal(3)
+        case _ => fail("wrong type")
       }
+    }
+  }
+
+  "updateScope" should {
+    "update scope calls increment functions" in {
+      val s = mock[IScope]
+
+      factory.updateScope(s)
+
+      verify(s, times(1)).incrementFuncs
     }
   }
 }
