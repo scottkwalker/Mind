bf9b9d074e62681ea065e5deeb2163b1ac63984d
Scott Walker <scott.walker@valtech.co.uk>
2014-06-17 23:23:47 +0100

    Given up on the memo technique from stackoverflow so cutting it out

--- app/nodes/legalNeighbours/LegalNeighboursImpl.scala
+++ app/nodes/legalNeighbours/LegalNeighboursImpl.scala
@@ -1,11 +1,14 @@
 package nodes.legalNeighbours
 
+import nodes.IntegerMFactory
 import nodes.helpers.{ICreateChildNodes, IScope}
 import com.google.inject.Inject
 import nodes.memoization.{Memoize, Memoize1}
 import play.api.libs.json.{Json, JsValue, Writes}
 import java.util.concurrent.CountDownLatch
 import play.api.libs.json.Json._
+import scala.annotation.tailrec
+import scala.collection.mutable
 
 final class LegalNeighboursImpl @Inject()(intToFactory: FactoryIdToFactory) extends LegalNeighbours {
   implicit val mapWrites = new Writes[Map[IScope, Either[CountDownLatch, Seq[Int]]]] {
@@ -25,24 +28,80 @@ final class LegalNeighboursImpl @Inject()(intToFactory: FactoryIdToFactory) exte
     }
   }
 
-  private def legalForScope(scope: IScope, neighbours: Seq[Int]): Seq[Int] = {
-    val memo: Memoize1[IScope, Seq[Int]] = {
-      def inner(f: Memoize1[IScope, Seq[Int]])(innerScope: IScope): Seq[Int] = {
-        if (innerScope.hasDepthRemaining) neighbours.filter {
-          neighbourId =>
-            val factory = intToFactory.convert(neighbourId)
-            factory.neighbourIds.isEmpty || legalForScope(scope = innerScope.incrementDepth, neighbours = factory.neighbourIds).length > 0
-        }
-        else Seq.empty
+  override def fetch(scope: IScope, neighbours: Seq[Int]): Seq[ICreateChildNodes] = legalForScope2(scope, neighbours).map(intToFactory.convert)
+
+
+
+  private[this] val cache = mutable.Map.empty[IScope, Either[CountDownLatch, Seq[Int]]]
+
+  /**
+   * What to do if we do not find the value already in the memo
+   * table.
+   */
+  @tailrec private[this] def missing(key: IScope, neighbours: Seq[Int]): Seq[Int] = {
+    synchronized {
+      // With the lock, check to see what state the value is in.
+      cache.get(key) match {
+        case None =>
+          // If it's missing, then claim the slot by putting in a CountDownLatch that will be completed when the value is
+          // available.
+          val latch = new CountDownLatch(1)
+          cache += (key -> Left(latch))
+
+          // The latch wrapped in Left indicates that the value needs to be computed in this thread, and then the
+          // latch counted down.
+          Left(latch)
+        case Some(other) =>
+          // This is either the latch that will indicate that the work has been done, or the computed value.
+          Right(other)
       }
+    } match {
+      case Right(Right(computed)) => computed // The computation is already done.
+      case Right(Left(latch)) =>
+        // Someone else is doing the computation.
+        latch.await()
+        // This recursive call will happen when there is an exception computing the value, or if the value is
+        // currently being computed.
+        missing(key, neighbours)
+      case Left(latch) =>
+        // Compute the value outside of the synchronized block.
+        val calculated =
+          try {
+            f(key, neighbours)
+          } catch {
+            case t: Throwable =>
+              // If there was an exception running the computation, then we need to make sure we do not
+              // starve any waiters before propagating the exception.
+              synchronized {
+                cache -= key
+              }
+              latch.countDown()
+              throw t
+          }
 
-      Memoize.Y(inner)
+        // Update the memo table to indicate that the work has been done, and signal to any waiting threads that the
+        // work is complete.
+        synchronized {
+          cache += (key -> Right(calculated))
+        }
+        latch.countDown()
+        calculated
     }
-
-    memo(scope.incrementDepth).intersect(neighbours) // Only return legal moves that are neighbours
   }
 
-  override def fetch(scope: IScope, neighbours: Seq[Int]): Seq[ICreateChildNodes] = legalForScope(scope, neighbours).map(intToFactory.convert)
+  def f(scope: IScope, neighbours: Seq[Int]): Seq[Int] = {
+    if (scope.hasDepthRemaining) neighbours.filter {
+      neighbourId =>
+        val factory = intToFactory.convert(neighbourId)
+        factory.neighbourIds.isEmpty || missing(key = scope.incrementDepth, neighbours = factory.neighbourIds).length > 0
+    }
+    else Seq.empty
+  }
+
+  private def legalForScope2(key: IScope, neighbours: Seq[Int]): Seq[Int] = {
+    cache.get(key) match {
+      case Some(Right(b)) => b
+      case _ => f(key, neighbours)
+    }
+  }
 }
-
-
