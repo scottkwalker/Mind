21e30e5c549a5d1f932361fb6ed4fc83b13cb6bd
Scott Walker <scott.walker@valtech.co.uk>
2014-08-11 22:27:36 +0100

    Didn't need constant on interface

--- app/models/domain/common/Node.scala
+++ app/models/domain/common/Node.scala
@@ -11,7 +11,7 @@ trait Node {
 
   def replaceEmpty(scope: IScope)(implicit injector: Injector): Node
 
-  def getMaxDepth: Int = 0
+  def getMaxDepth: Int
 
   def updateScope(scope: IScope): IScope
 }
