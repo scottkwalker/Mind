517832e50190007f852e1e250a264630d6592b04
Scott Walker <scott.walker@valtech.co.uk>
2014-08-11 08:55:46 +0100

    set tests to pending

--- test/fitness/GeneratorSpec.scala
+++ test/fitness/GeneratorSpec.scala
@@ -5,17 +5,12 @@ import utils.helpers.UnitSpec
 final class GeneratorSpec extends UnitSpec {
 
   "run" should {
-    "get ai instance" in {
-      //true shouldEqual false
-    }
+    "get ai instance" in pending
 
-    "return best solution and its fitness" in {
-    }
+    "return best solution and its fitness" in pending
 
-    "loop the specified number of times" in {
-    }
+    "loop the specified number of times" in pending
 
-    "stop when threshold fitness reached" in {
-    }
+    "stop when threshold fitness reached" in pending
   }
 }
