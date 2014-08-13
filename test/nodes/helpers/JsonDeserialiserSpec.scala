24fb62524993f627cdc8c5df377caca8725980e5
Scott Walker <scott.walker@valtech.co.uk>
2014-08-13 20:11:08 +0100

    code coverage for JsonDeserialiserSpec

--- test/nodes/helpers/JsonDeserialiserSpec.scala
+++ test/nodes/helpers/JsonDeserialiserSpec.scala
@@ -1,13 +1,29 @@
 package nodes.helpers
 
+import models.domain.common.JsonValidationException
+import nodes.legalNeighbours.BitSetAdapter.readJsonBitMaskToBitset
+import play.api.libs.json.Json
 import utils.helpers.UnitSpec
+import scala.collection.immutable.BitSet
 
 final class JsonDeserialiserSpec extends UnitSpec {
 
   "deserialize" should {
-    "throws JsonValidationException when JSON is invalid" in pending /*{
-      val j = mock[JsValue]
-      a[JsonValidationException] should be thrownBy JsonDeserialiser.deserialize(j)
-    }*/
+    "throws JsonValidationException when json is invalid" in {
+      val invalidBitmaskJson = """{"bitMask":["INVALID"]}"""
+      a[JsonValidationException] should be thrownBy JsonDeserialiser.deserialize(invalidBitmaskJson)
+    }
+
+    "return model when string is valid json" in {
+      val dataAsBitSet = BitSet.empty + 3 + 4 + 4 + 100 + 101
+      val bitMaskAsJson = """{"bitMask":[24,206158430208]}"""
+      JsonDeserialiser.deserialize(bitMaskAsJson) should equal(dataAsBitSet)
+    }
+
+    "return model when JsValue is valid json" in {
+      val dataAsBitSet = BitSet.empty + 3 + 4 + 4 + 100 + 101
+      val bitMaskAsJson = Json.parse( """{"bitMask":[24,206158430208]}""")
+      JsonDeserialiser.deserialize(bitMaskAsJson) should equal(dataAsBitSet)
+    }
   }
 }
