3f24bb4bee48d6f66b67a4fb0f5437ba6bd093f0
Scott Walker <scott.walker@valtech.co.uk>
2014-08-13 20:25:38 +0100

    test coverage for FactoryIdToFactoryImpl

--- test/nodes/legalNeighbours/FactoryIdToFactoryImplSpec.scala
+++ test/nodes/legalNeighbours/FactoryIdToFactoryImplSpec.scala
@@ -1,5 +1,6 @@
 package nodes.legalNeighbours
 
+import nodes._
 import utils.helpers.UnitSpec
 
 final class FactoryIdToFactoryImplSpec extends UnitSpec {
@@ -8,5 +9,28 @@ final class FactoryIdToFactoryImplSpec extends UnitSpec {
     "throw when id is not in the list" in {
       a[RuntimeException] should be thrownBy injector.getInstance(classOf[FactoryIdToFactory]).convert(-1)
     }
+
+    "return expected factory" in {
+      factoryIdToFactory.convert(NodeTreeFactoryImpl.id) should equal(nodeTreeFactory)
+    }
+  }
+
+  private val nodeTreeFactory = mock[NodeTreeFactory]
+  private val factoryIdToFactory = {
+    val addOperatorFactory = mock[AddOperatorFactory]
+    val functionMFactory = mock[FunctionMFactory]
+    val integerMFactory = mock[IntegerMFactoryImpl]
+    val objectDefFactory = mock[ObjectDefFactory]
+    val valDclInFunctionParamFactory = mock[ValDclInFunctionParamFactory]
+    val valueRefFactory = mock[ValueRefFactory]
+    new FactoryIdToFactoryImpl(
+      addOperatorFactory,
+      functionMFactory,
+      integerMFactory,
+      nodeTreeFactory,
+      objectDefFactory,
+      valDclInFunctionParamFactory,
+      valueRefFactory
+    )
   }
 }
