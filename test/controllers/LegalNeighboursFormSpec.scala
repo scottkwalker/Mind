7ef721e8f6984056c00d40bae04e45f71ef3f05b
Scott Walker <scott.walker@valtech.co.uk>
2014-08-18 21:07:19 +0100

    form test for wrong types

--- test/controllers/LegalNeighboursFormSpec.scala
+++ test/controllers/LegalNeighboursFormSpec.scala
@@ -10,8 +10,43 @@ final class LegalNeighboursFormSpec extends UnitSpec {
       val errors = legalNeighbours.form.bind(Map("" -> "")).errors
       errors.length should equal(8)
     }
+
+    "reject when submission contains wrong types" in {
+      val errors = formWithValidDefaults(
+        numVals = "INVALID",
+        numFuncs = "INVALID",
+        numObjects = "INVALID",
+        height = "INVALID",
+        maxExpressionsInFunc = "INVALID",
+        maxFuncsInObject = "INVALID",
+        maxParamsInFunc = "INVALID",
+        maxObjectsInTree = "INVALID"
+      ).errors
+      errors.length should equal(8)
+    }
   }
 
+  private def formWithValidDefaults(numVals: String = "0",
+                                    numFuncs: String = "0",
+                                    numObjects: String = "0",
+                                    height: String = "0",
+                                    maxExpressionsInFunc: String = "0",
+                                    maxFuncsInObject: String = "0",
+                                    maxParamsInFunc: String = "0",
+                                    maxObjectsInTree: String = "0") = {
+    legalNeighbours.form.bind(
+      Map(
+        numValsId -> numVals,
+        numFuncsId -> numFuncs,
+        numObjectsId -> numObjects,
+        heightId -> height,
+        maxExpressionsInFuncId -> maxExpressionsInFunc,
+        maxFuncsInObjectId -> maxFuncsInObject,
+        maxParamsInFuncId -> maxParamsInFunc,
+        maxObjectsInTreeId -> maxObjectsInTree
+      )
+    )
+  }
 
   private val legalNeighbours = injector.getInstance(classOf[LegalNeighbours])
 }
