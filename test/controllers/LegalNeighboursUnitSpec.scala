2a298aea956240ddcf8bcbe15db80c753c21c4ac
Scott Walker <scott.walker@valtech.co.uk>
2014-08-18 21:45:41 +0100

    controller test ok response

--- test/controllers/LegalNeighboursUnitSpec.scala
+++ test/controllers/LegalNeighboursUnitSpec.scala
@@ -1,6 +1,7 @@
 package controllers
 
-import play.api.test.Helpers.BAD_REQUEST
+import models.common.Scope.Form._
+import play.api.test.Helpers.{BAD_REQUEST, OK}
 import play.api.test.{FakeRequest, WithApplication}
 import utils.helpers.UnitSpec
 
@@ -8,13 +9,44 @@ final class LegalNeighboursUnitSpec extends UnitSpec {
 
   "calculate" should {
     "return bad request when submission is empty" in new WithApplication {
-      val request = FakeRequest().withFormUrlEncodedBody()
-      val result = legalNeighbours.calculate(request)
+      val emptyRequest = FakeRequest().withFormUrlEncodedBody()
+      val result = legalNeighbours.calculate(emptyRequest)
       whenReady(result) { r =>
         r.header.status should equal(BAD_REQUEST)
       }
     }
+
+    "return ok when submission is valid" in new WithApplication {
+      val validRequest = requestWithDefaults()
+      val result = legalNeighbours.calculate(validRequest)
+      whenReady(result) { r =>
+        r.header.status should equal(OK)
+      }
+    }
+
+    "return ok with seq of ids when submission is valid" in pending
   }
 
   private val legalNeighbours = injector.getInstance(classOf[LegalNeighbours])
+
+  private def requestWithDefaults(
+                       numVals: String = "1",
+                       numFuncs: String = "2",
+                       numObjects: String = "3",
+                       height: String = "4",
+                       maxExpressionsInFunc: String = "5",
+                       maxFuncsInObject: String = "6",
+                       maxParamsInFunc: String = "7",
+                       maxObjectsInTree: String = "8"
+                       ) =
+    FakeRequest().withFormUrlEncodedBody(
+      s"$numValsId" -> numVals,
+      s"$numFuncsId" -> numFuncs,
+      s"$numObjectsId" -> numObjects,
+      s"$heightId" -> height,
+      s"$maxExpressionsInFuncId" -> maxExpressionsInFunc,
+      s"$maxFuncsInObjectId" -> maxFuncsInObject,
+      s"$maxParamsInFuncId" -> maxParamsInFunc,
+      s"$maxObjectsInTreeId" -> maxObjectsInTree
+    )
 }
