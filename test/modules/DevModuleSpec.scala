94e4ca1ebf9162be42da9a42bd2e7cb2e96e5462
Scott Walker <scott.walker@valtech.co.uk>
2014-08-19 22:35:26 +0100

    Added pending tests

---
+++ test/modules/DevModuleSpec.scala
@@ -0,0 +1,14 @@
+package modules
+
+import utils.helpers.UnitSpec
+
+final class DevModuleSpec extends UnitSpec {
+
+  "configure" should {
+    "create an empty memoization map when saved file does not exist" in pending
+
+    "create an empty memoization map when saved file exist but versioning does not match" in pending
+
+    "populate the memoization map when a saved file exists with matching versioning" in pending
+  }
+}
