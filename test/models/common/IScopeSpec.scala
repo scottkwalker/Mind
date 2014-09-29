62847068d3fce931030f517ecdf087ee74973dcc
Scott Walker <scott.walker@valtech.co.uk>
2014-09-29 08:19:00 +0100

    Represent json as JsValues to confirm correctness before stringifying

--- test/models/common/IScopeSpec.scala
+++ test/models/common/IScopeSpec.scala
@@ -27,11 +27,28 @@ final class IScopeSpec extends TestComposition {
   }
 
   "deserialize" must {
-    "return expected model" in {
+    "return expected model when input is a JsValue" in {
       JsonDeserialiser.deserialize[IScope](asJson) must equal(asModel)
+    }
+
+    "return expected model when input is a String" in {
+      JsonDeserialiser.deserialize[IScope](asString) must equal(asModel)
     }
   }
 
-  val asJson = """{"numVals":0,"numFuncs":0,"numObjects":0,"height":0,"maxExpressionsInFunc":0,"maxFuncsInObject":0,"maxParamsInFunc":0,"height":0,"maxObjectsInTree":0}"""
-  val asModel: IScope = Scope()
+  private val asJson = JsObject(
+    fields = Seq(
+      ("numVals", JsNumber(0)),
+      ("numFuncs", JsNumber(0)),
+      ("numObjects", JsNumber(0)),
+      ("height", JsNumber(0)),
+      ("maxExpressionsInFunc", JsNumber(0)),
+      ("maxFuncsInObject", JsNumber(0)),
+      ("maxParamsInFunc", JsNumber(0)),
+      ("height", JsNumber(0)),
+      ("maxObjectsInTree", JsNumber(0))
+    )
+  )
+  private val asString = Json.stringify(asJson)
+  private val asModel: IScope = Scope()
 }
