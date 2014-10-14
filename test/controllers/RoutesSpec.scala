db8ce9c9c1ea0c5b95a41f0cab53698d7d4ccfda
Scott Walker <scott.walker@valtech.co.uk>
2014-10-14 21:15:45 +0100

    Closes #2: test the route as in the scaltest plus play docs

--- test/controllers/RoutesSpec.scala
+++ test/controllers/RoutesSpec.scala
@@ -7,8 +7,29 @@ import play.api.test.Helpers._
 final class RoutesSpec extends TestComposition {
 
   "Routes" must {
+
+    "define GET /" in new App {
+      val Some(result) = route(FakeRequest(GET, "/"))
+      result must be('defined)
+    }
+
+    "define GET /mind/intro" in new App {
+      val Some(result) = route(FakeRequest(GET, "/mind/intro"))
+      result must be('defined)
+    }
+
     "define GET /mind/health-check" in new App {
       val Some(result) = route(FakeRequest(GET, "/mind/health-check"))
+      result must be('defined)
+    }
+
+    "define GET /mind/legal-neighbours" in new App {
+      val Some(result) = route(FakeRequest(GET, "/mind/legal-neighbours"))
+      result must be('defined)
+    }
+
+    "define POST /mind/legal-neighbours" in new App {
+      val Some(result) = route(FakeRequest(POST, "/mind/legal-neighbours"))
       result must be('defined)
     }
   }
