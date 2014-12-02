ba743867bb9bcd086411496c6753aff85b1fc078
Scott Walker <scott.walker@valtech.co.uk>
2014-12-02 08:08:23 +0000

    Renames

--- test/models/domain/scala/ValDclInFunctionParamSpec.scala
+++ test/models/domain/scala/ValDclInFunctionParamSpec.scala
@@ -13,94 +13,94 @@ final class ValDclInFunctionParamSpec extends TestComposition {
 
   "toRawScala" must {
     "return expected" in {
-      val p = mock[Instruction]
-      when(p.toRaw).thenReturn("Int")
+      val instruction = mock[Instruction]
+      when(instruction.toRaw).thenReturn("Int")
       val name = "a"
 
-      ValDclInFunctionParam(name, p).toRaw must equal("a: Int")
+      ValDclInFunctionParam(name, instruction).toRaw must equal("a: Int")
     }
   }
 
   "hasNoEmpty" must {
     "false given it cannot terminate in under N steps" in {
-      val s = mock[IScope]
-      when(s.hasHeightRemaining).thenReturn(false)
+      val scope = mock[IScope]
+      when(scope.hasHeightRemaining).thenReturn(false)
       val name = "a"
-      val p = mock[Instruction]
+      val instruction = mock[Instruction]
 
-      ValDclInFunctionParam(name, p).hasNoEmpty(s) must equal(false)
+      ValDclInFunctionParam(name, instruction).hasNoEmpty(scope) must equal(false)
     }
 
     "false given an empty name" in {
-      val s = mock[IScope]
-      when(s.hasHeightRemaining).thenReturn(true)
+      val scope = mock[IScope]
+      when(scope.hasHeightRemaining).thenReturn(true)
       val name = ""
-      val p = mock[Instruction]
+      val instruction = mock[Instruction]
 
-      ValDclInFunctionParam(name, p).hasNoEmpty(s) must equal(false)
+      ValDclInFunctionParam(name, instruction).hasNoEmpty(scope) must equal(false)
     }
 
     "false given an invalid child" in {
-      val s = mock[IScope]
-      when(s.hasHeightRemaining).thenReturn(true)
+      val scope = mock[IScope]
+      when(scope.hasHeightRemaining).thenReturn(true)
       val name = "a"
-      val p = mock[Instruction]
-      when(p.hasNoEmpty(any[Scope])).thenReturn(false)
+      val instruction = mock[Instruction]
+      when(instruction.hasNoEmpty(any[Scope])).thenReturn(false)
 
-      ValDclInFunctionParam(name, p).hasNoEmpty(s) must equal(false)
+      ValDclInFunctionParam(name, instruction).hasNoEmpty(scope) must equal(false)
     }
 
     "true given it can terminate, has a non-empty name and valid child" in {
-      val s = mock[IScope]
-      when(s.hasHeightRemaining).thenReturn(true)
+      val scope = mock[IScope]
+      when(scope.hasHeightRemaining).thenReturn(true)
       val name = "a"
-      val p = IntegerM()
+      val instruction = IntegerM()
 
-      ValDclInFunctionParam(name, p).hasNoEmpty(s) must equal(true)
+      ValDclInFunctionParam(name, instruction).hasNoEmpty(scope) must equal(true)
     }
   }
 
   "replaceEmpty" must {
     "calls replaceEmpty on non-empty child nodes" in {
-      val s = mock[IScope]
-      when(s.incrementVals).thenReturn(s)
-      when(s.decrementHeight).thenReturn(s)
+      val scope = mock[IScope]
+      when(scope.incrementVals).thenReturn(scope)
+      when(scope.decrementHeight).thenReturn(scope)
       val name = "a"
-      implicit val i = mock[Injector]
-      val p = mock[Instruction]
-      when(p.replaceEmpty(any[Scope])(any[Injector])) thenReturn Future.successful(p)
-      val instance = ValDclInFunctionParam(name, p)
+      val injector = mock[Injector]
+      val instruction = mock[Instruction]
+      when(instruction.replaceEmpty(any[Scope])(any[Injector])) thenReturn Future.successful(instruction)
+      val valDclInFunctionParam = ValDclInFunctionParam(name, instruction)
 
-      val result = instance.replaceEmpty(s)
+      val result = valDclInFunctionParam.replaceEmpty(scope)(injector)
 
-      whenReady(result) { _ => verify(p, times(1)).replaceEmpty(any[Scope])(any[Injector])}
+      whenReady(result) { _ => verify(instruction, times(1)).replaceEmpty(any[Scope])(any[Injector])}
     }
 
     "returns same when no empty nodes" in {
-      val s = mock[IScope]
-      when(s.incrementVals).thenReturn(s)
-      when(s.decrementHeight).thenReturn(s)
+      val scope = mock[IScope]
+      when(scope.incrementVals).thenReturn(scope)
+      when(scope.decrementHeight).thenReturn(scope)
       val name = "a"
-      implicit val i = mock[Injector]
-      val p = mock[Instruction]
-      when(p.replaceEmpty(any[Scope])(any[Injector])) thenReturn Future.successful(p)
-      val instance = ValDclInFunctionParam(name, p)
+      val injector = mock[Injector]
+      val instruction = mock[Instruction]
+      when(instruction.replaceEmpty(any[Scope])(any[Injector])) thenReturn Future.successful(instruction)
+      val valDclInFunctionParam = ValDclInFunctionParam(name, instruction)
 
-      val result = instance.replaceEmpty(s)
+      val result = valDclInFunctionParam.replaceEmpty(scope)(injector)
 
       whenReady(result) {
-        _ must equal(instance)
+        _ must equal(valDclInFunctionParam)
       }
     }
 
     "returns without empty nodes given there were empty nodes" in {
-      val s = mock[IScope]
+      val scope = mock[IScope]
       val name = "a"
       val primitiveTypeEmpty = Empty()
-      val i = testInjector(new StubReplaceEmpty)
-      val instance = ValDclInFunctionParam(name, primitiveTypeEmpty)
+      val injector = testInjector(new StubReplaceEmpty)
+      val valDclInFunctionParam = ValDclInFunctionParam(name, primitiveTypeEmpty)
 
-      val result = instance.replaceEmpty(s)(i)
+      val result = valDclInFunctionParam.replaceEmpty(scope)(injector)
 
       whenReady(result) {
         case ValDclInFunctionParam(name2, primitiveType) =>
@@ -114,10 +114,10 @@ final class ValDclInFunctionParamSpec extends TestComposition {
   "height" must {
     "returns 1 + child height" in {
       val name = "a"
-      val p = mock[Instruction]
-      when(p.height).thenReturn(1)
+      val instruction = mock[Instruction]
+      when(instruction.height).thenReturn(1)
 
-      ValDclInFunctionParam(name, p).height must equal(2)
+      ValDclInFunctionParam(name, instruction).height must equal(2)
     }
   }
 }
