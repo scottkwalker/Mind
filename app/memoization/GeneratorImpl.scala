6ea65d4588cb91d840c085d48255951433b40a39
Scott Walker <scott.walker@valtech.co.uk>
2015-01-05 22:31:04 +0000

    the minimum to get a green test that generator calls lookupChildren.fetch

--- app/memoization/GeneratorImpl.scala
+++ app/memoization/GeneratorImpl.scala
@@ -1,10 +1,15 @@
 package memoization
 
 import com.google.inject.Inject
+import models.common.Scope
+import utils.PozInt
 
 import scala.concurrent.Future
 
 class GeneratorImpl @Inject()(lookupChildren: LookupChildren) extends Generator {
 
-  def generate: Future[Boolean] = Future.successful(true)
+  def generate: Future[Boolean] = {
+    lookupChildren.fetch(Scope(), new PozInt(1))
+    Future.successful(true)
+  }
 }
--- test/memoization/GeneratorImplSpec.scala
+++ test/memoization/GeneratorImplSpec.scala
@@ -1,16 +1,32 @@
 package memoization
 
-import composition.TestComposition
+import composition.{StubLookupChildren, TestComposition}
+import models.common.IScope
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import utils.PozInt
 
 class GeneratorImplSpec extends TestComposition {
 
   "generate" must {
     "return true" in {
+      val (_, generator) = build
       whenReady(generator.generate) {
         _ must equal(true)
       }
     }
+
+    "call lookupChildren.fetch" in {
+      val (lookupChildren, generator) = build
+      whenReady(generator.generate) { r =>
+        verify(lookupChildren, times(1)).fetch(any[IScope], any[PozInt])
+      }
+    }
   }
 
-  private def generator = testInjector().getInstance(classOf[Generator])
+  private def build = {
+    val lookupChildren: LookupChildren = mock[LookupChildren]
+    val generator = testInjector(new StubLookupChildren(lookupChildren)).getInstance(classOf[Generator])
+    (lookupChildren, generator)
+  }
 }
