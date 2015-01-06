dfb8cdebfc1688eddb8adbc7045357202c5f65c4
Scott Walker <scott.walker@valtech.co.uk>
2015-01-06 20:35:10 +0000

    Looping over some scope max's

--- app/memoization/GeneratorImpl.scala
+++ app/memoization/GeneratorImpl.scala
@@ -8,9 +8,16 @@ import scala.concurrent.Future
 class GeneratorImpl @Inject()(lookupChildren: LookupChildren) extends Generator {
 
   def generate(maxScope: IScope): Future[Boolean] = {
-    // We can evaluate across all the types of factories in parallel as they all depend on results calculated
-    // further "down" the map, no dependencies on the same level.
-    lookupChildren.factoryLookup.factories.par.map(id => lookupChildren.fetch(maxScope, id))
+    for {
+      _ <- 0 to maxScope.maxExpressionsInFunc
+      _ <- 0 to maxScope.maxFuncsInObject
+      _ <- 0 to maxScope.maxParamsInFunc
+      _ <- 0 to maxScope.maxObjectsInTree
+    } yield {
+      // We can evaluate across all the types of factories in parallel as they all depend on results calculated
+      // further "down" the map, no dependencies on the same level.
+      lookupChildren.factoryLookup.factories.par.map(id => lookupChildren.fetch(maxScope, id))
+    }
 
     Future.successful(true)
   }
--- test/memoization/GeneratorImplSpec.scala
+++ test/memoization/GeneratorImplSpec.scala
@@ -18,21 +18,49 @@ class GeneratorImplSpec extends TestComposition {
       }
     }
 
-    "call lookupChildren.fetch once for each factory (when scope allows only one call)" in {
-      val (lookupChildren, generator) = build
+    "call lookupChildren.fetch once for each factory (when scope has no values)" in {
       val scope = mock[IScope]
-      whenReady(generator.generate(scope)) { r =>
-        verify(lookupChildren, times(3)).fetch(any[IScope], any[PozInt])
-      }
+      verfifyLookupChildrenCalled(scope, expected = 1)
+    }
+
+    "call lookupChildren.fetch once for each scope maxExpressionsInFunc" in {
+      val scope = mock[IScope]
+      when(scope.maxExpressionsInFunc).thenReturn(1)
+      verfifyLookupChildrenCalled(scope)
+    }
+
+    "call lookupChildren.fetch once for each scope maxFuncsInObject" in {
+      val scope = mock[IScope]
+      when(scope.maxFuncsInObject).thenReturn(1)
+      verfifyLookupChildrenCalled(scope)
+    }
+
+    "call lookupChildren.fetch once for each scope maxParamsInFunc" in {
+      val scope = mock[IScope]
+      when(scope.maxParamsInFunc).thenReturn(1)
+      verfifyLookupChildrenCalled(scope)
+    }
+
+    "call lookupChildren.fetch once for each scope maxObjectsInTree" in {
+      val scope = mock[IScope]
+      when(scope.maxObjectsInTree).thenReturn(1)
+      verfifyLookupChildrenCalled(scope)
     }
   }
 
   private def build = {
     val factoryLookup: FactoryLookup = mock[FactoryLookup]
-    when(factoryLookup.factories).thenReturn(Set(new PozInt(0), new PozInt(1), new PozInt(2)))
+    when(factoryLookup.factories).thenReturn(Set(new PozInt(0)))
     val lookupChildren: LookupChildren = mock[LookupChildren]
     when(lookupChildren.factoryLookup).thenReturn(factoryLookup)
     val generator = testInjector(new StubLookupChildren(lookupChildren)).getInstance(classOf[Generator])
     (lookupChildren, generator)
+  }
+
+  private def verfifyLookupChildrenCalled(scope: IScope, expected: Int = 2) = {
+    val (lookupChildren, generator) = build
+    whenReady(generator.generate(scope)) { r =>
+      verify(lookupChildren, times(expected)).fetch(any[IScope], any[PozInt])
+    }
   }
 }
