56583a00c96335231ebf821b7d8c52e10c5b0d73
Scott Walker <scott.walker@valtech.co.uk>
2015-01-06 23:06:40 +0000

    each step in the for loop is in parallel except height. Need to test that we aren't overkill with threads, may be better if we somehow made a stream of Scopes

--- app/memoization/GeneratorImpl.scala
+++ app/memoization/GeneratorImpl.scala
@@ -10,20 +10,21 @@ class GeneratorImpl @Inject()(lookupChildren: LookupChildren) extends Generator 
   def generate(maxScope: IScope): Future[Boolean] = {
     for {
       height <- 0 to maxScope.height // Must be first val in the loop so that all lookups for one height are done in one go.
-      _ <- 0 to maxScope.maxExpressionsInFunc // THIS IS NOT USED SO SHOULD BE REMOVED WITH OTHERS FROM THE MAPPING AND HTML
-      numFuncs <- 0 to maxScope.maxFuncsInObject
-      numVals <- 0 to maxScope.maxParamsInFunc
-      numObjects <- 0 to maxScope.maxObjectsInTree
-    } yield {
-      val currentScope = Scope(
+      _ <- (0 to maxScope.maxExpressionsInFunc).par // THIS IS NOT USED SO SHOULD BE REMOVED WITH OTHERS FROM THE MAPPING AND HTML
+      numFuncs <- (0 to maxScope.maxFuncsInObject).par // TODO check that doing this in parallel is not drowning us with threads
+      numVals <- (0 to maxScope.maxParamsInFunc).par
+      numObjects <- (0 to maxScope.maxObjectsInTree).par
+      currentScope: IScope = Scope(
         numVals = numVals,
         numFuncs = numFuncs,
         numObjects = numObjects,
         height = height
       )
+      id <- lookupChildren.factoryLookup.factories.par
+    } {
       // We can evaluate across all the types of factories in parallel as they all depend on results calculated
       // further "down" the map, no dependencies on the same level.
-      lookupChildren.factoryLookup.factories.par.map(id => lookupChildren.fetch(currentScope, id))
+      lookupChildren.fetch(currentScope, id)
     }
 
     Future.successful(true)
