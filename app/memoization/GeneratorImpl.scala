f3ae025659947ed29d1d33fc6e5632446515473e
Scott Walker <scott.walker@valtech.co.uk>
2015-01-06 20:19:17 +0000

    A little bit of parallel

--- app/memoization/GeneratorImpl.scala
+++ app/memoization/GeneratorImpl.scala
@@ -2,16 +2,16 @@ package memoization
 
 import com.google.inject.Inject
 import models.common.IScope
-import utils.PozInt
 
 import scala.concurrent.Future
 
 class GeneratorImpl @Inject()(lookupChildren: LookupChildren) extends Generator {
 
   def generate(maxScope: IScope): Future[Boolean] = {
-    for(id <- lookupChildren.factoryLookup.factories) {
-      lookupChildren.fetch(maxScope, id)
-    }
+    // We can evaluate across all the types of factories in parallel as they all depend on results calculated
+    // further "down" the map, no dependencies on the same level.
+    lookupChildren.factoryLookup.factories.par.map(id => lookupChildren.fetch(maxScope, id))
+
     Future.successful(true)
   }
 }
