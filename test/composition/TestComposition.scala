126a18acc791d32f3f4e6f30f205687bcc41d7ed
Scott Walker <scott.walker@valtech.co.uk>
2015-02-02 20:55:46 +0000

    scale ScalaTest interval and timeout so that it allows more time on travis-CI

--- test/composition/TestComposition.scala
+++ test/composition/TestComposition.scala
@@ -5,8 +5,8 @@ import com.google.inject.{Guice, Module}
 import composition.ai.legalGamer.LegalGamerModule
 import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.time.{Milliseconds, Seconds, Span}
 import org.scalatestplus.play.PlaySpec
+
 
 import scala.concurrent.duration.{FiniteDuration, SECONDS}
 
@@ -15,6 +15,14 @@ trait TestComposition extends PlaySpec with MockitoSugar with ScalaFutures with 
   protected val finiteTimeout = FiniteDuration(2, SECONDS)
   protected implicit val timeout = akka.util.Timeout(duration = finiteTimeout)
   private val defaultModules = Seq(new DevModule, new LegalGamerModule)
+
+  // The default PatienceConfig from ScalaTest uses scaled time spans. The interval and timeout for the PatienceConfig
+  // will be scaled, allowing us to run the tests with fail-fast values for local testing or high values for testing
+  // on Continuous Integration servers (which tend to be much slower).
+  // http://doc.scalatest.org/2.2.1/index.html#org.scalatest.concurrent.ScaledTimeSpans
+  // We will read spanScaleFactor from system properties (if one exists, otherwise use the default). This system
+  // property should be overridden in the SBT command line argument in a build server's build script e.g. .travis.yml
+  override def spanScaleFactor: Double = sys.props.getOrElse("spanScaleFactor", "1.0").toDouble
 
   def testInjector(modules: Module*) = {
     // The modules override in such a way that if you declare the same binding twice, the last write wins.
