c1c56b011eada97cd74214bab8297d1aa0f9e444
Scott Walker <scott.walker@valtech.co.uk>
2015-02-11 22:24:52 +0000

    Move GlobalLike out of Global into package composition

---
+++ app/composition/GlobalLike.scala
@@ -0,0 +1,51 @@
+package composition
+
+import java.io.File
+
+import com.typesafe.config.ConfigFactory
+import filters.WithFilters
+import play.api.Application
+import play.api.Configuration
+import play.api.GlobalSettings
+import play.api.Mode
+
+trait GlobalLike extends WithFilters with GlobalSettings with Composition {
+
+  /**
+   * Application configuration is in a hierarchy of files:
+   *
+   * application.conf
+   * /             |            \
+   * application.prod.conf    application.dev.conf    application.test.conf <- these can override and add to application.conf
+   *
+   * play test  <- test mode picks up application.test.conf
+   * play run   <- dev mode picks up application.dev.conf
+   * play start <- prod mode picks up application.prod.conf
+   *
+   * To override and stipulate a particular "conf" e.g.
+   * play -Dconfig.file=conf/application.test.conf run
+   */
+  override def onLoadConfig(configuration: Configuration,
+                            path: File,
+                            classloader: ClassLoader,
+                            mode: Mode.Mode): Configuration = {
+    val applicationConf = System.getProperty("config.file", s"application.${mode.toString.toLowerCase}.conf")
+    val environmentOverridingConfiguration = configuration ++
+      Configuration(ConfigFactory.load(applicationConf))
+    super.onLoadConfig(environmentOverridingConfiguration, path, classloader, mode)
+  }
+
+  /**
+   * Controllers must be resolved through the application context. There is a special method of GlobalSettings
+   * that we can override to resolve a given controller. This resolution is required by the Play router.
+   */
+  override def getControllerInstance[A](controllerClass: Class[A]): A = injector.getInstance(controllerClass)
+
+  override def onStart(app: Application): Unit = {
+    super.onStart(app)
+  }
+
+  override def onStop(app: Application): Unit = {
+    super.onStop(app)
+  }
+}
