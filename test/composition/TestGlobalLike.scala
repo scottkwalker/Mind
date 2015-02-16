e7594e62004b261d5f4bd7812e603c970bea6977
Scott Walker <scott.walker@valtech.co.uk>
2015-02-16 23:38:38 +0000

    comment

--- test/composition/TestGlobalLike.scala
+++ test/composition/TestGlobalLike.scala
@@ -6,6 +6,7 @@ import play.api.GlobalSettings
 
 trait TestGlobalLike extends WithFilters with GlobalSettings {
 
+  // Use mixin to define the values in the injector that will be used for IoC.
   val injector: Injector
 
   // Routing leads to this function that instantiates the controller and at the same time injects any dependencies as
