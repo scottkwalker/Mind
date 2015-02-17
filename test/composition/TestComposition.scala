837d1cba6a0800e1daa0f5a2ec4cab2ea35fe973
Scott Walker <scott.walker@valtech.co.uk>
2015-02-17 21:15:34 +0000

    made testInjector protected, tidy up

--- test/composition/TestComposition.scala
+++ test/composition/TestComposition.scala
@@ -12,6 +12,12 @@ trait TestComposition extends Composition {
 
   override lazy val injector: Injector = testInjector()
 
+  protected def testInjector(modules: Module*) = {
+    // The modules override in such a way that if you declare the same binding twice, the last write wins.
+    val overridenModules = `override`(defaultModules: _*).`with`(modules: _*)
+    Guice.createInjector(overridenModules)
+  }
+
   // Modules that stub most of the application's dependencies. This should be enough for most UI tests.
   private def defaultModules = Seq(
     new TestModule,
@@ -28,12 +34,6 @@ trait TestComposition extends Composition {
     new StubRngBinding,
     new StubSelectionStrategyBinding
   )
-
-  def testInjector(modules: Module*) = {
-    // The modules override in such a way that if you declare the same binding twice, the last write wins.
-    val overridenModules = `override`(defaultModules: _*).`with`(modules: _*)
-    Guice.createInjector(overridenModules)
-  }
 
   private final class TestModule extends AbstractModule {
 
